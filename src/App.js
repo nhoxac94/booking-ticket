c7717c04724431ffb16fcaae735b23df2552c3f1
Le Hoang Loi <nhoxac94@yahoo.com>
2021-09-02 21:38:49 +0700

    add layout trang dat ve

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import FilmDetail from 'containers/client/Home/FilmDetail/FilmDetail'
+import SeatDetail from 'containers/client/Home/SeatDetail/SeatDetail'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-            <FilmDetail/>
+            <SeatDetail/>
         </Switch>
       </Router>
     </div>
