90ab91f4164e8534fde6ddc2c2e10c20460722cf
Le Hoang Loi <nhoxac94@yahoo.com>
2021-09-02 09:17:19 +0700

    FimDetailLayout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import FilmDetail from 'containers/client/Home/FilmDetail/FilmDetail'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-
+            <FilmDetail/>
         </Switch>
       </Router>
     </div>
