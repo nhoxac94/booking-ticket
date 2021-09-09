f153b5a4fae84610486756780431679319b82600
Quang <buiminhquang188@gmail.com>
2021-09-09 11:58:10 +0700

    Add Admin layout, MovieManager and AddMovie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import AdminLayout from 'layouts/AdminLayout';
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
-import AddUser from 'containers/admin/Admin/UserManagement/AddUser/AddUser';
 function App() {
   return (
     <Router>
       <Switch>
-        <AddUser />
+        <AdminLayout />
       </Switch>
     </Router>
   );
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,14 @@ import React, { Component } from "react";
 
 export default class Button extends Component {
   render() {
-    return <button className='btn btn-success mx-2'>{this.props.children}</button>;
+    return (
+      <button
+        className={`btn btn-info ${this.props.styling}`}
+        data-toggle={this.props.dataToggle}
+        data-target={this.props.dataTarget}
+      >
+        {this.props.children}
+      </button>
+    );
   }
 }
