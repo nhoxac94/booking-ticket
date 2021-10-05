f8bd3a7ef6a7431e038482717cd9776d3ea4be91
Quang <buiminhquang188@gmail.com>
2021-10-05 12:02:53 +0700

    fix footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,10 +11,10 @@ export default class Footer extends Component {
     return (
       <footer className="footer">
         <div className="container mx-auto">
-          <div className="row text-white py-4">
+          <div className="row text-white py-4 justify-content-between align-self-center">
             <div className="col-lg-4 col-6">
               <p className="h4">123Phim</p>
-              <div className="d-flex justify-content-start ">
+              <div className="d-flex justify-content-lg-start justify-content-center ">
                 <div className="footer__faq">
                   <ul>
                     <li>FAQ</li>
@@ -34,27 +34,29 @@ export default class Footer extends Component {
             <div className="col-lg-4 col-6">
               <p className="h4">Đối tác</p>
               <div className="container">
-                <div className="row align-items-center flex-wrap footer__collab">
+                <div className="row flex-wrap footer__collab">
                   <div className="col">
-                    <img
-                      src="https://picsum.photos/id/1/50/50"
-                      alt=""
-                      className="rounded-circle"
-                    />
+                    <div className="d-flex justify-content-center justify-content-lg-start ">
+                      <img
+                        src="https://picsum.photos/id/1/50/50"
+                        alt=""
+                        className="rounded-circle"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
             <div className="col-lg-2 col-6">
               <p className="h4">Mobile App</p>
-              <div className="d-flex justify-content-start">
+              <div className="d-flex justify-content-lg-start justify-content-center">
                 <AndroidOutlined style={{ fontSize: "2rem" }} />
                 <MobileOutlined style={{ fontSize: "2rem" }} />
               </div>
             </div>
             <div className="col-lg-2 col-6">
               <p className="h4">Social</p>
-              <div className="d-flex justify-content-start">
+              <div className="d-flex justify-content-lg-start justify-content-center">
                 <FacebookOutlined style={{ fontSize: "2rem" }} />
                 <InstagramOutlined style={{ fontSize: "2rem" }} />
               </div>
